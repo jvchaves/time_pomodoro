2a3d7d7064a17ba830813d115f1986ec64a98604
João Victor Chaves <joao.victor.chaves159@gmail.com>
2024-05-13 16:51:28 -0300

    Chore:Reformulo estrutura de paginas

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>Logo</span>
+      <nav>
+        <a href=""> TIMER </a>
+        <a href=""> HISTORY </a>
+      </nav>
+    </HeaderContainer>
+  )
+}
